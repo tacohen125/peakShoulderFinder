a65039e869b771d4daf6139f07d7562fa8f89bd2
Ted Cohen <tacohen@uw.edu>
2018-05-17 14:56:56 -0700

    UNIT TESTSgit add test_peak_shoulder_finder.py Ted_Testing.ipynb !

--- test_peak_shoulder_finder.py
+++ test_peak_shoulder_finder.py
@@ -4,6 +4,7 @@ import peak_shoulder_finder as pks
 
 def test_index_return():
 
+	#generates a test case
 	pts=25 
 
 	x=np.linspace(-5,5,num=pts)
@@ -13,7 +14,12 @@ def test_index_return():
 	yactual_3 = -(0.5* (x+2))**2
 	yactual = yactual_1 + yactual_2 + yactual_3
 
+	#runs the function
 	inflection_points_index, peak_index = pks.index_return(yactual)
 
+	assert isinstance(inflection_points_index, list), 'the inflection point output is not a list'
+	assert isinstance(peak_index, list), 'the peak point output is not a list'
 
+	assert all(isinstance(item, int) for item in inflection_points_index), 'the inflection indices are not integers'
+	
 	return
