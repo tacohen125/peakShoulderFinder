d5c8d23713e222c99834162b5666027c025e5dd2
Aoi <aoi@joindrift.com>
2018-05-17 18:06:00 -0700

    edits to formatting

--- psf/tests/test_peak_shoulder_finder.py
+++ psf/tests/test_peak_shoulder_finder.py
@@ -15,21 +15,31 @@ def test_index_return():
 	yactual = yactual_1 + yactual_2 + yactual_3
 
 	#runs the function
-	inflection_points_index, peak_index = psf.index_return(yactual)
+	inflection_points_index, peak_index = psf.index_return(yactual,
+			returnInflection=False, returnPeak=True)
 
-	assert isinstance(inflection_points_index, list), 'the inflection point output is not a list'
+	assert (isinstance(inflection_points_index, list),
+				'the inflection point output is not a list')
 	assert isinstance(peak_index, list), 'the peak point output is not a list'
 
-	assert all(isinstance(item, int) for item in inflection_points_index), 'the inflection indices are not integers'
-	assert all(isinstance(item, int) for item in peak_index), 'the peak indices are not integers'
+	assert (all(isinstance(item, int) for item in inflection_points_index),
+				'the inflection indices are not integers')
+	assert (all(isinstance(item, int) for item in peak_index),
+				'the peak indices are not integers')
 
-	assert all(item >= 0 for item in inflection_points_index), 'the inflection indices are not positive'
-	assert all(item >= 0 for item in peak_index), 'the peak indices are not positive'
+	assert (all(item >= 0 for item in inflection_points_index),
+				'the inflection indices are not positive')
+	assert (all(item >= 0 for item in peak_index),
+				'the peak indices are not positive')
 
-	assert (max(inflection_points_index) <= len(yactual)-1), 'the inflection point output contains an index that is too large'
-	assert (max(peak_index) <= len(yactual)-1), 'the peak point output contains an index that is too large'
+	assert ((max(inflection_points_index) <= len(yactual)-1),
+			'the inflection point output contains an index that is too large')
+	assert ((max(peak_index) <= len(yactual)-1),
+			'the peak point output contains an index that is too large')
 
-	assert (len(inflection_points_index) == 2), 'the inflection points list is not the right size'
-	assert (len(peak_index) == 1), 'the peak points list is not the right size'
+	assert ((len(inflection_points_index) == 2), 
+			'the inflection points list is not the right size')
+	assert ((len(peak_index) == 1),
+			'the peak points list is not the right size')
 
 	return
